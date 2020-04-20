5e7d6d498cca2e39e0c4c5e839edf63c40e32987
jisshub <jissmon476@gmail.com>
2020-04-20 15:45:37 +0530

    recipee-bok-component - template- outyputted each item in recipee array

--- src/app/recipees/recipee-book/recipee-book.component.ts
+++ src/app/recipees/recipee-book/recipee-book.component.ts
@@ -13,6 +13,8 @@ export class RecipeeBookComponent implements OnInit {
   recipeeList: Recipee[] = [
     // instantiate Recipee class - pass arguments
     new Recipee("a test recipee for all", "a delicious recipee for all", 
+    "https://www.andiemitchell.com/wp-content/uploads/2011/01/baked-buffalo-chicken-rolls-1-680x477.jpg"),
+    new Recipee("a test recipee for all", "a delicious recipee for all", 
     "https://www.andiemitchell.com/wp-content/uploads/2011/01/baked-buffalo-chicken-rolls-1-680x477.jpg")
   ];
   constructor() { }
