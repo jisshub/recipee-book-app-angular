01bfd536530c6cf35c78d3e51726e90c9d2e0cb6
jisshub <jissmon476@gmail.com>
2020-04-20 21:17:30 +0530

    new

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ShoppingListComponent implements OnInit {
   public ingredients: string[];
     constructor() {
-      
+
      }
 
   ngOnInit(): void {
