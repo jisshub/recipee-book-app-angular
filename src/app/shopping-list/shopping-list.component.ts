810acec86bc27a347a2a2bedfde083f3de8738cf
jissmon jose <jissmon476@gmail.com>
2020-04-21 09:13:31 +0530

    footer added - shopping list component template

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+// import Ingredient class from 
+import { Ingredient } from "../shared/ingredient.model";
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  public ingredients: string[];
+  
+    // create an ingredients array of type Ingredient class
+    ingredients: Ingredient[]=[
+      new Ingredient("fsdas", 123),
+      new Ingredient("dgdsd", 223),
+    ]
     constructor() {
 
      }
