592b09f3b0d3b41f363428504fe84cc1b6ebac93
jisshub <jissmon476@gmail.com>
2020-05-11 16:53:35 +0530

    added ingredient to recipees

--- src/app/recipees/recipee.service.ts
+++ src/app/recipees/recipee.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from "@angular/core";
 import { Recipee } from "../recipees/recipees.model";
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable() export class RecipeeService {
 
@@ -9,10 +10,13 @@ import { Recipee } from "../recipees/recipees.model";
     private recipeeList: Recipee[] = [
         // instantiate Recipee class - pass arguments
         new Recipee("a test recipee for all", "a delicious recipee for all", 
-        "https://www.andiemitchell.com/wp-content/uploads/2011/01/baked-buffalo-chicken-rolls-1-680x477.jpg"),
+        "https://www.andiemitchell.com/wp-content/uploads/2011/01/baked-buffalo-chicken-rolls-1-680x477.jpg", [new Ingredient("sdasd", '23213'), new Ingredient("asdsad", "2432")]),
+
         new Recipee("a test recipee for all", "a delicious recipee for all", 
-        "https://www.andiemitchell.com/wp-content/uploads/2011/01/baked-buffalo-chicken-rolls-1-680x477.jpg"),
-        new Recipee("a new test recipee", "lorem ipsum", "https://www.andiemitchell.com/wp-content/uploads/2011/01/baked-buffalo-chicken-rolls-1-680x477.jpg")
+        "https://www.andiemitchell.com/wp-content/uploads/2011/01/baked-buffalo-chicken-rolls-1-680x477.jpg", [new Ingredient("das", "22"), new Ingredient("adas", "123")]),
+
+        new Recipee("a new test recipee", "lorem ipsum", "https://www.andiemitchell.com/wp-content/uploads/2011/01/baked-buffalo-chicken-rolls-1-680x477.jpg", [new Ingredient('asdasd', '333'),
+        new Ingredient("adasd", "33")])
       ];
 
       getRecipee(){
